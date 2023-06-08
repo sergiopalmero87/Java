569ecba813cd658a5c5d9354452cc4f74bff1ec1
sergiopalmero <sergiopalmero87@gmail.com>
2023-06-08 16:30:12 +0200

    CoversorUnidades

---
+++ ConvertirUnidades/src/Conversor.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Conversor {
+
+	public static void main(String[] args) {
+		// Conversor de unidades: Desarrolla un programa que convierta unidades de medida, como metros millas.
+		// El usuario debe ingresar el valor y la unidad de entrada, y el programa debe realizar la conversión.
+		
+		
+		Scanner leer = new Scanner(System.in);
+		int metros;
+		Conversor conversor = new Conversor();
+		
+		
+		System.out.println("Escribe los metros que quieres convertir a millas: ");
+		
+		metros = leer.nextInt();
+		
+
+		System.out.println("El resultado de tu consulta es que " + metros+ " metros " + conversor.convertir(metros));
+		
+	}
+	
+	private String convertir(int metros) {
+		
+		int result = metros / 1609;
+		return "son " + result + " millas";
+	}
+
+}
